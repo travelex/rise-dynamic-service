2986f45b6c4d4af278a19eaae04a04c187f8fd1b
Siddhiraj Pantoji <Siddhiraj.Pantoji@travelex.com>
2022-06-13 01:02:59 +0530

    Changes for Require

--- rise-event-sub/api-operations/file-success-handler/src/service/StatsService.js
+++ rise-event-sub/api-operations/file-success-handler/src/service/StatsService.js
@@ -34,7 +34,16 @@ class StatsService {
                 let fileContent = await S3Service.getObjectData(bucketName,fileName)
                 fileContent = fileContent.toString("utf-8")
                 logger.debug("File Content = "+fileContent)
+                const currentPath = __dirname;
+				logger.debug(`current path :: ${currentPath}`);
+				const childNodeModulePath = currentPath.substring(0, currentPath.lastIndexOf('src')) + 'node_modules';
+				logger.debug(`Child Node Module Path :: ${childNodeModulePath}`);
+				const opts = { prependPaths: [childNodeModulePath], logFilter: () => false };
+				let ruleObject = await reqFromMem.requireFromString(fileContent, __dirname + `/${filePath}`, opts);
                 
+				logger.debug('Evaluating  rule');
+
+				logger.debug('Apply  rule');
             }
         } catch (error) {
             logger.error(`Error occurred while fetching connection: ${JSON.stringify(error)}`);
